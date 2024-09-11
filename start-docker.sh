d22ab9e3267dcb29cf5b3da1b3491b5ed9a0f7e3
Dr. Jusseaux <35258413+maepopi@users.noreply.github.com>
2024-09-11 13:51:58 +0200

    added dev line in top to force the docker image to use latest scripts without rebuilding

--- start-docker.sh
+++ start-docker.sh
@@ -5,20 +5,24 @@ docker run \
     --rm \
     --gpus all \
     --name ai-voice-cloning \
-    -v "${PWD}/models:/home/user/ai-voice-cloning/models" \
-    -v "${PWD}/training:/home/user/ai-voice-cloning/training" \
-    -v "${PWD}/voices:/home/user/ai-voice-cloning/voices" \
+    -v "${PWD}/src:/home/user/ai-voice-cloning/src" \
+    -v "/home/maelys/AI_PROJECTS/SOUND/TOOLS/MRQ/ai-voice-cloning/models:/home/user/ai-voice-cloning/models" \
+    -v "/home/maelys/AI_PROJECTS/SOUND/TOOLS/MRQ/ai-voice-cloning/training:/home/user/ai-voice-cloning/training" \
+    -v "/home/maelys/AI_PROJECTS/SOUND/TOOLS/MRQ/ai-voice-cloning/voices:/home/user/ai-voice-cloning/voices" \
     -v "${PWD}/bin:/home/user/ai-voice-cloning/bin" \
     -v "${PWD}/config:/home/user/ai-voice-cloning/config" \
+    -v "/home/maelys/AI_PROJECTS/SOUND/TOOLS/Jarod_MRQ/ai-voice-cloning/results:/home/user/ai-voice-cloning/results" \
     --user "$(id -u):$(id -g)" \
     -p "7860:7860" \
-    ai-voice-cloning $@
+    ai-voice-cloning $@  # If pulled from Docker Hub, replace with doctorpopi/ai-voice-cloning
 
 # For dev:
-#     -v "${PWD}/src:/home/user/ai-voice-cloning/src" \
-#     -v "${PWD}/modules/tortoise_dataset_tools/dataset_whisper_tools:/home/user/ai-voice-cloning/modules/tortoise_dataset_tools/dataset_whisper_tools" \
-#     -v "${PWD}/modules/dlas/dlas:/home/user/ai-voice-cloning/modules/dlas/dlas" \
-#     -v "/home/user/ai-voice-cloning/src/__pycache__" \
+# These lines allow the docker image to pull the latest script changes without rebuilding the image
+#     -v "${PWD}/src:/home/user/ai-voice-cloning/src"
+#     -v "${PWD}/modules/tortoise_dataset_tools/dataset_whisper_tools:/home/user/ai-voice-cloning/modules/tortoise_dataset_tools/dataset_whisper_tools"
+#     -v "${PWD}/modules/dlas/dlas:/home/user/ai-voice-cloning/modules/dlas/dlas"
+#     -v "/home/user/ai-voice-cloning/src/__pycache__"
 
 # For testing:
-#    -e "PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True" \
+#    -e "PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True"
+
